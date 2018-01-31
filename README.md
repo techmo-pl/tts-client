473d047ccf3b429acb4c7b3e5c04d710d1362dfc
Paweł Jaciów <pawel.jaciow@techmo.pl>
2018-01-31 16:45:02 +0100

    Added Python client.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Tribune TTS gRPC client
 Contents:  
 - `proto`   Protocol Buffers API definition.  
 - `cpp`     Tribune TTS gRPC client C++ implementation.  
+- `python`  Tribune TTS gRPC client Python implementation.  
 - `tools`   Utilities. Scripts for dependencies installation.  
 
 Language-specific build instructions can be found in their respective directories.
@@ -32,4 +33,7 @@ You can set `SynthesizeConfig`'s fields to specify parameters of synthesis. Curr
 
 `Synthesize` returns synthesized audio in `SynthesizeResponse` as a stream. When reading from the stream you have to check if `SynthesizeResponse` contains `error` field. If it does you can print its `code` and `description`. No `error` field in `SynthesizeResponse` means everything worked fine and its `audio` contains byte `content` that can be appended to received audio samples with `sample_rate_hertz` sampling frequency in hertz. When receiving `SynthesizeResponse` with `audio` you have to check if its `end_of_stream` flag is set to true. When it is set to true it means service has fnished synthesis and you can save your wave file with received synthesized audio content.
 
-We provide sample TTS Client written in C++ in `cpp`. The example accepts text to be synthesized as a command line string. By default it saves "TechmoTTS.wav" file with received synthesized audio content. To use it, you have to specify provided by us service IP address and port using `--service-address` option with string in form "address:port".
+We provide sample TTS Client written in:
+- C++ in `cpp` (accepts text to be synthesized as a command line string),  
+- Python in `python` (accepts text to be synthesized as a command line string or as a content of given text file).  
+By default it saves "TechmoTTS.wav" file with received synthesized audio content. To use it, you have to specify provided by us service IP address and port using `--service-address` option with string in form "address:port".
---
+++ python/tribune_client.py
@@ -0,0 +1,72 @@
+import tribune_tts_pb2
+import tribune_tts_pb2_grpc
+import grpc
+import os
+from wave_saver import WaveSaver
+from argparse import ArgumentParser
+import codecs
+from VERSION import TRIBUNE_CLIENT_VERSION
+
+def main():
+    print("Tribune TTS gRPC client " + TRIBUNE_CLIENT_VERSION)
+
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--service-address", dest="service", required=True,
+                  help="IP address and port (address:port) of a service the client will connect to.", type=str)
+    parser.add_argument("-t", "--text", dest="text", default="Techmo Trybun: Syntezator mowy polskiej.",
+                  help="Text to be synthesized (in polish).", type=str)
+    parser.add_argument("-i", "--input_text_file", dest="inputfile", default="",
+                  help="A file with text to be synthesized (in polish).", type=str) 
+    parser.add_argument("-o", "--out-path", dest="out_path", default="TechmoTTS.wav",
+                   help="Path to output wave file with synthesized audio content.", type=str)
+    parser.add_argument("-f", "--sample_rate", dest="sample_rate", default=44100,
+                  help="Sample rate in Hz of synthesized audio.", type=int)
+    # Parse and validate options
+    args = parser.parse_args()
+
+    # Check if service address and port are provided
+    if len(args.service) == 0:
+        raise RuntimeError("No service address and port provided.")
+
+    # Input text determination
+    input_text = ""
+    if len(args.inputfile) > 0:
+        with codecs.open(args.inputfile, encoding='utf-8', mode="r") as fread:  
+            input_text = fread.read()
+    elif len(args.text) > 0:
+        input_text = args.text  
+    else:
+        raise RuntimeError("Empty input string for synthesis.")
+                    
+    # Output file determination
+    wavefilename = os.path.join(args.out_path)
+    
+    # Establish GRPC channel
+    channel = grpc.insecure_channel(args.service)
+    stub = tribune_tts_pb2_grpc.TTSStub(channel)
+
+    # Synthesis request
+    config = tribune_tts_pb2.SynthesizeConfig(sample_rate_hertz=int(args.sample_rate))
+    request = tribune_tts_pb2.SynthesizeRequest(text=input_text, config=config)
+    ws = WaveSaver()
+    try:
+        for response in stub.Synthesize(request):
+            if response.HasField('error'):
+                print("Error [" + str(response.error.code) + "]: " + response.error.description)
+                break
+            else:
+                if ws._framerate:
+                    if ws._framerate != response.audio.sample_rate_hertz:
+                        raise RuntimeError("Sample rate does not match previously received.")
+                else:
+                    ws.setFrameRate(response.audio.sample_rate_hertz)
+                ws.append(response.audio.content)
+                if response.audio.end_of_stream:
+                    ws.save(wavefilename)
+    except grpc.RpcError as e:
+        print("[Server-side error] Received following RPC error from the TTS service:", str(e))
+    ws.clear()
+
+
+if __name__ == '__main__':
+    main()
