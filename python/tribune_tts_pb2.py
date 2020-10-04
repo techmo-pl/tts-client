fa5703150757dbc37aab31a5dbbc2a4e602e1d65
Michal Radziszewski <michal.radziszewski@techmo.pl>
2020-10-05 00:02:44 +0200

    updated autogenerated python proto files

--- python/tribune_tts_pb2.py
+++ python/tribune_tts_pb2.py
@@ -21,9 +21,82 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='techmo.tribune',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x11tribune_tts.proto\x12\x0etechmo.tribune\"S\n\x11SynthesizeRequest\x12\x0c\n\x04text\x18\x01 \x01(\t\x12\x30\n\x06\x63onfig\x18\x02 \x01(\x0b\x32 .techmo.tribune.SynthesizeConfig\"-\n\x10SynthesizeConfig\x12\x19\n\x11sample_rate_hertz\x18\x01 \x01(\x05\"d\n\x12SynthesizeResponse\x12(\n\x05\x61udio\x18\x01 \x01(\x0b\x32\x19.techmo.tribune.AudioData\x12$\n\x05\x65rror\x18\x02 \x01(\x0b\x32\x15.techmo.tribune.Error\"N\n\tAudioData\x12\x19\n\x11sample_rate_hertz\x18\x01 \x01(\x05\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\x0c\x12\x15\n\rend_of_stream\x18\x03 \x01(\x08\"E\n\x05\x45rror\x12\'\n\x04\x63ode\x18\x01 \x01(\x0e\x32\x19.techmo.tribune.ErrorCode\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t*_\n\tErrorCode\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0b\n\x07LICENCE\x10\x01\x12\x16\n\x12TEXT_NORMALIZATION\x10\x02\x12\x11\n\rTRANSCRIPTION\x10\x03\x12\r\n\tSYNTHESIS\x10\x04\x32\\\n\x03TTS\x12U\n\nSynthesize\x12!.techmo.tribune.SynthesizeRequest\x1a\".techmo.tribune.SynthesizeResponse0\x01\x62\x06proto3')
+  serialized_pb=_b('\n\x11tribune_tts.proto\x12\x0etechmo.tribune\"S\n\x11SynthesizeRequest\x12\x0c\n\x04text\x18\x01 \x01(\t\x12\x30\n\x06\x63onfig\x18\x02 \x01(\x0b\x32 .techmo.tribune.SynthesizeConfig\"\x9a\x01\n\x10SynthesizeConfig\x12\x19\n\x11sample_rate_hertz\x18\x01 \x01(\x05\x12\x33\n\routput_format\x18\x02 \x01(\x0e\x32\x1c.techmo.tribune.OutputFormat\x12\x10\n\x08language\x18\x03 \x01(\t\x12$\n\x05voice\x18\x04 \x01(\x0b\x32\x15.techmo.tribune.Voice\"_\n\x05Voice\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x06gender\x18\x02 \x01(\x0e\x32\x16.techmo.tribune.Gender\x12 \n\x03\x61ge\x18\x03 \x01(\x0e\x32\x13.techmo.tribune.Age\"d\n\x12SynthesizeResponse\x12(\n\x05\x61udio\x18\x01 \x01(\x0b\x32\x19.techmo.tribune.AudioData\x12$\n\x05\x65rror\x18\x02 \x01(\x0b\x32\x15.techmo.tribune.Error\"N\n\tAudioData\x12\x19\n\x11sample_rate_hertz\x18\x01 \x01(\x05\x12\x0f\n\x07\x63ontent\x18\x02 \x01(\x0c\x12\x15\n\rend_of_stream\x18\x03 \x01(\x08\"E\n\x05\x45rror\x12\'\n\x04\x63ode\x18\x01 \x01(\x0e\x32\x19.techmo.tribune.ErrorCode\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t*)\n\x0cOutputFormat\x12\t\n\x05PCM16\x10\x00\x12\x0e\n\nOGG_VORBIS\x10\x01*$\n\x03\x41ge\x12\t\n\x05\x41\x44ULT\x10\x00\x12\t\n\x05\x43HILD\x10\x01\x12\x07\n\x03OLD\x10\x02*\x1e\n\x06Gender\x12\n\n\x06\x46\x45MALE\x10\x00\x12\x08\n\x04MALE\x10\x01*_\n\tErrorCode\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0b\n\x07LICENCE\x10\x01\x12\x16\n\x12TEXT_NORMALIZATION\x10\x02\x12\x11\n\rTRANSCRIPTION\x10\x03\x12\r\n\tSYNTHESIS\x10\x04\x32\\\n\x03TTS\x12U\n\nSynthesize\x12!.techmo.tribune.SynthesizeRequest\x1a\".techmo.tribune.SynthesizeResponse0\x01\x62\x06proto3')
 )
 
+_OUTPUTFORMAT = _descriptor.EnumDescriptor(
+  name='OutputFormat',
+  full_name='techmo.tribune.OutputFormat',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='PCM16', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='OGG_VORBIS', index=1, number=1,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=629,
+  serialized_end=670,
+)
+_sym_db.RegisterEnumDescriptor(_OUTPUTFORMAT)
+
+OutputFormat = enum_type_wrapper.EnumTypeWrapper(_OUTPUTFORMAT)
+_AGE = _descriptor.EnumDescriptor(
+  name='Age',
+  full_name='techmo.tribune.Age',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='ADULT', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='CHILD', index=1, number=1,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='OLD', index=2, number=2,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=672,
+  serialized_end=708,
+)
+_sym_db.RegisterEnumDescriptor(_AGE)
+
+Age = enum_type_wrapper.EnumTypeWrapper(_AGE)
+_GENDER = _descriptor.EnumDescriptor(
+  name='Gender',
+  full_name='techmo.tribune.Gender',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='FEMALE', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='MALE', index=1, number=1,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=710,
+  serialized_end=740,
+)
+_sym_db.RegisterEnumDescriptor(_GENDER)
+
+Gender = enum_type_wrapper.EnumTypeWrapper(_GENDER)
 _ERRORCODE = _descriptor.EnumDescriptor(
   name='ErrorCode',
   full_name='techmo.tribune.ErrorCode',
@@ -53,12 +126,19 @@ _ERRORCODE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=422,
-  serialized_end=517,
+  serialized_start=742,
+  serialized_end=837,
 )
 _sym_db.RegisterEnumDescriptor(_ERRORCODE)
 
 ErrorCode = enum_type_wrapper.EnumTypeWrapper(_ERRORCODE)
+PCM16 = 0
+OGG_VORBIS = 1
+ADULT = 0
+CHILD = 1
+OLD = 2
+FEMALE = 0
+MALE = 1
 UNKNOWN = 0
 LICENCE = 1
 TEXT_NORMALIZATION = 2
@@ -119,6 +199,27 @@ _SYNTHESIZECONFIG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='output_format', full_name='techmo.tribune.SynthesizeConfig.output_format', index=1,
+      number=2, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='language', full_name='techmo.tribune.SynthesizeConfig.language', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='voice', full_name='techmo.tribune.SynthesizeConfig.voice', index=3,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -131,8 +232,53 @@ _SYNTHESIZECONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=122,
-  serialized_end=167,
+  serialized_start=123,
+  serialized_end=277,
+)
+
+
+_VOICE = _descriptor.Descriptor(
+  name='Voice',
+  full_name='techmo.tribune.Voice',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='techmo.tribune.Voice.name', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='gender', full_name='techmo.tribune.Voice.gender', index=1,
+      number=2, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='age', full_name='techmo.tribune.Voice.age', index=2,
+      number=3, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=279,
+  serialized_end=374,
 )
 
 
@@ -169,8 +315,8 @@ _SYNTHESIZERESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=169,
-  serialized_end=269,
+  serialized_start=376,
+  serialized_end=476,
 )
 
 
@@ -214,8 +360,8 @@ _AUDIODATA = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=271,
-  serialized_end=349,
+  serialized_start=478,
+  serialized_end=556,
 )
 
 
@@ -252,19 +398,27 @@ _ERROR = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=351,
-  serialized_end=420,
+  serialized_start=558,
+  serialized_end=627,
 )
 
 _SYNTHESIZEREQUEST.fields_by_name['config'].message_type = _SYNTHESIZECONFIG
+_SYNTHESIZECONFIG.fields_by_name['output_format'].enum_type = _OUTPUTFORMAT
+_SYNTHESIZECONFIG.fields_by_name['voice'].message_type = _VOICE
+_VOICE.fields_by_name['gender'].enum_type = _GENDER
+_VOICE.fields_by_name['age'].enum_type = _AGE
 _SYNTHESIZERESPONSE.fields_by_name['audio'].message_type = _AUDIODATA
 _SYNTHESIZERESPONSE.fields_by_name['error'].message_type = _ERROR
 _ERROR.fields_by_name['code'].enum_type = _ERRORCODE
 DESCRIPTOR.message_types_by_name['SynthesizeRequest'] = _SYNTHESIZEREQUEST
 DESCRIPTOR.message_types_by_name['SynthesizeConfig'] = _SYNTHESIZECONFIG
+DESCRIPTOR.message_types_by_name['Voice'] = _VOICE
 DESCRIPTOR.message_types_by_name['SynthesizeResponse'] = _SYNTHESIZERESPONSE
 DESCRIPTOR.message_types_by_name['AudioData'] = _AUDIODATA
 DESCRIPTOR.message_types_by_name['Error'] = _ERROR
+DESCRIPTOR.enum_types_by_name['OutputFormat'] = _OUTPUTFORMAT
+DESCRIPTOR.enum_types_by_name['Age'] = _AGE
+DESCRIPTOR.enum_types_by_name['Gender'] = _GENDER
 DESCRIPTOR.enum_types_by_name['ErrorCode'] = _ERRORCODE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -281,6 +435,13 @@ SynthesizeConfig = _reflection.GeneratedProtocolMessageType('SynthesizeConfig', 
   # @@protoc_insertion_point(class_scope:techmo.tribune.SynthesizeConfig)
   })
 _sym_db.RegisterMessage(SynthesizeConfig)
+
+Voice = _reflection.GeneratedProtocolMessageType('Voice', (_message.Message,), {
+  'DESCRIPTOR' : _VOICE,
+  '__module__' : 'tribune_tts_pb2'
+  # @@protoc_insertion_point(class_scope:techmo.tribune.Voice)
+  })
+_sym_db.RegisterMessage(Voice)
 
 SynthesizeResponse = _reflection.GeneratedProtocolMessageType('SynthesizeResponse', (_message.Message,), {
   'DESCRIPTOR' : _SYNTHESIZERESPONSE,
@@ -311,8 +472,8 @@ _TTS = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=519,
-  serialized_end=611,
+  serialized_start=839,
+  serialized_end=931,
   methods=[
   _descriptor.MethodDescriptor(
     name='Synthesize',
--- python/tribune_tts_pb2_grpc.py
+++ python/tribune_tts_pb2_grpc.py
@@ -7,26 +7,23 @@ import tribune_tts_pb2 as tribune__tts__pb2
 class TTSStub(object):
   """Service that implements Techmo Text-To-Speech (TTS) API.
 
-  Service's `Synthesize` method accepts `SynthesizeRequest` object which contains whole phrase to be synthesized.  
-  You have to put the phrase as a string in `text` field of `SynthesizeRequest`. The string has to be in orthographic form. In that string you can use several special tags which can be interpreted. Tags have to be in from `<tag>something special</tag>` and can occur in any place in text. Currently interpreted tags are:
+  Service's `Synthesize` method accepts `SynthesizeRequest` object which contains the whole phrase to be synthesized.  
+  The phrase to synthesize is put as a string in `text` field of `SynthesizeRequest`. The string has to be in orthographic form.
+  The string can be either a plain text or SSML (https://w3.org/TR/speech-synthesis11/).
+  Currently the following SSML tags are supported:
+  `<speak>` - root xml node, with optional `xml:lang` attribute,
+  `<prosody>` - supported attributes: `pitch`, `range`, `rate`, and `volume`,
+  `<break>` - supported attributes: `strength` and `time`,
+  `<emphasis>` - supported attribute: `level`,
+  `<say-as>` - supported attribute: `interpret-as` (consult Techmo TTS documentation for the complete list of all available implementations),
+  `<lang>` - supported attribute: `xml:lang`,
+  `<voice>` - supported attributes: `name`, `gender`, and `age`.
 
-  | Tag | Description | Example (input) | Example (output) |
-  | --- | ----------- | --------------- | ---------------- |
-  | **cardinal** | *cardinal number* | `<cardinal>7</cardinal>` | *siedem* |
-  | **signed** | *number with sign* | `<signed>-15</signed>` | *minus piętnaście* |
-  | **ordinal** | *ordinal number* | `<ordinal>1</ordinal>` | *pierwszy* |
-  | **fraction** | *fractional number* | `<fraction>3/4</fraction>` | *trzy czwarte* |
-  | **postal** | *postal code* | `<postal>30-020</postal>` | *trzydzieści zero dwadzieścia* |
-  | **time** | *time* | `<time>22:00</time>` | *dwudziesta druga* |
-  | **date** | *date* | `<date>12/05/2001</date>` | *dwunasty maja dwa tysiące jeden* |
-
-  Note: when interpreting tags only nominal case is supported at the moment.
-
-  You can set `SynthesizeConfig`'s fields to specify parameters of synthesis. Currently supported option is only `sample_rate_hertz`, which is desired sampling frequency (in hertz) of synthesized audio.
+  `SynthesizeConfig`'s fields can be set to specify parameters of synthesis (sampling rate, language, and voice) and a format of the output (PCM16 or Ogg/Vorbis compression).
 
   `SynthesizeRequest` can be sent to the service via gRPC insecure channel (that does not require authentication).
 
-  `Synthesize` returns synthesized audio in `SynthesizeResponse` as a stream. When reading from the stream you have to check if `SynthesizeResponse` contains `error` field. If it does you can print its `code` and `description`. No `error` field in `SynthesizeResponse` means everything worked fine and its `audio` contains byte `content` that can be appended to received audio samples with `sample_rate_hertz` sampling frequency in hertz. When receiving `SynthesizeResponse` with `audio` you have to check if its `end_of_stream` flag is set to true. When it is set to true it means service has fnished synthesis and you can save your wave file with received synthesized audio content.
+  `Synthesize` returns synthesized audio in `SynthesizeResponse` as a stream. When reading from the stream it is necessary to check if `SynthesizeResponse` contains `error` field. If it does its `code` and `description` can be printed. No `error` field in `SynthesizeResponse` means everything worked fine and its `audio` contains byte `content` that can be appended to received audio samples with `sample_rate_hertz` sampling frequency in hertz. When receiving In `SynthesizeResponse` with `audio`, the `end_of_stream` set to true means that service has fnished synthesis. Otherwise further chunks with more audio are expected.
   """
 
   def __init__(self, channel):
@@ -45,26 +42,23 @@ class TTSStub(object):
 class TTSServicer(object):
   """Service that implements Techmo Text-To-Speech (TTS) API.
 
-  Service's `Synthesize` method accepts `SynthesizeRequest` object which contains whole phrase to be synthesized.  
-  You have to put the phrase as a string in `text` field of `SynthesizeRequest`. The string has to be in orthographic form. In that string you can use several special tags which can be interpreted. Tags have to be in from `<tag>something special</tag>` and can occur in any place in text. Currently interpreted tags are:
+  Service's `Synthesize` method accepts `SynthesizeRequest` object which contains the whole phrase to be synthesized.  
+  The phrase to synthesize is put as a string in `text` field of `SynthesizeRequest`. The string has to be in orthographic form.
+  The string can be either a plain text or SSML (https://w3.org/TR/speech-synthesis11/).
+  Currently the following SSML tags are supported:
+  `<speak>` - root xml node, with optional `xml:lang` attribute,
+  `<prosody>` - supported attributes: `pitch`, `range`, `rate`, and `volume`,
+  `<break>` - supported attributes: `strength` and `time`,
+  `<emphasis>` - supported attribute: `level`,
+  `<say-as>` - supported attribute: `interpret-as` (consult Techmo TTS documentation for the complete list of all available implementations),
+  `<lang>` - supported attribute: `xml:lang`,
+  `<voice>` - supported attributes: `name`, `gender`, and `age`.
 
-  | Tag | Description | Example (input) | Example (output) |
-  | --- | ----------- | --------------- | ---------------- |
-  | **cardinal** | *cardinal number* | `<cardinal>7</cardinal>` | *siedem* |
-  | **signed** | *number with sign* | `<signed>-15</signed>` | *minus piętnaście* |
-  | **ordinal** | *ordinal number* | `<ordinal>1</ordinal>` | *pierwszy* |
-  | **fraction** | *fractional number* | `<fraction>3/4</fraction>` | *trzy czwarte* |
-  | **postal** | *postal code* | `<postal>30-020</postal>` | *trzydzieści zero dwadzieścia* |
-  | **time** | *time* | `<time>22:00</time>` | *dwudziesta druga* |
-  | **date** | *date* | `<date>12/05/2001</date>` | *dwunasty maja dwa tysiące jeden* |
-
-  Note: when interpreting tags only nominal case is supported at the moment.
-
-  You can set `SynthesizeConfig`'s fields to specify parameters of synthesis. Currently supported option is only `sample_rate_hertz`, which is desired sampling frequency (in hertz) of synthesized audio.
+  `SynthesizeConfig`'s fields can be set to specify parameters of synthesis (sampling rate, language, and voice) and a format of the output (PCM16 or Ogg/Vorbis compression).
 
   `SynthesizeRequest` can be sent to the service via gRPC insecure channel (that does not require authentication).
 
-  `Synthesize` returns synthesized audio in `SynthesizeResponse` as a stream. When reading from the stream you have to check if `SynthesizeResponse` contains `error` field. If it does you can print its `code` and `description`. No `error` field in `SynthesizeResponse` means everything worked fine and its `audio` contains byte `content` that can be appended to received audio samples with `sample_rate_hertz` sampling frequency in hertz. When receiving `SynthesizeResponse` with `audio` you have to check if its `end_of_stream` flag is set to true. When it is set to true it means service has fnished synthesis and you can save your wave file with received synthesized audio content.
+  `Synthesize` returns synthesized audio in `SynthesizeResponse` as a stream. When reading from the stream it is necessary to check if `SynthesizeResponse` contains `error` field. If it does its `code` and `description` can be printed. No `error` field in `SynthesizeResponse` means everything worked fine and its `audio` contains byte `content` that can be appended to received audio samples with `sample_rate_hertz` sampling frequency in hertz. When receiving In `SynthesizeResponse` with `audio`, the `end_of_stream` set to true means that service has fnished synthesis. Otherwise further chunks with more audio are expected.
   """
 
   def Synthesize(self, request, context):
