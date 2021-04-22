72eef595031e546d14d0e087a7f2d042433491ae
Jan Wozniak <jan.wozniak@techmo.pl>
2021-04-22 08:05:59 +0200

    Refactoring setup script

--- python/setup.sh
+++ python/setup.sh
@@ -4,6 +4,31 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+
+check_package () {
+    # $1 - package name
+    # $2 - if == "sudo" use sudo
+
+    if [ $(dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        while true; do
+            read -p "The required package $1 is not installed. Do you want to install it now? [y/n]" yn
+            case $yn in
+                [Yy]*)
+                if [ $# -eq 2 ] && [ $2 == "sudo" ];
+                then
+                    sudo apt-get update && sudo apt-get install -y "$1"; 
+                else
+                    apt-get update && apt-get install -y "$1";
+                fi;
+                break ;;
+                [Nn]*) exit 0 ;;
+            esac
+        done
+    fi
+}
+
+
 # check Python version
 
 python3_missing="false"
@@ -28,87 +53,16 @@ fi
 if [ $(dpkg-query -W -f='${Status}' sudo 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
     #sudo not installed (eg. docker container)
-    if [ $(dpkg-query -W -f='${Status}' python3-dev 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'python3-dev' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) apt-get update && apt-get install -y python3-dev; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if [ $(dpkg-query -W -f='${Status}' libportaudio2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'libportaudio2' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) apt-get update && apt-get install -y libportaudio2; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if [ $(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'python3-pip' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) apt-get update && apt-get install -y python3-pip; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if ! python3 -c "import virtualenv" &> /dev/null; then
-        while true; do
-                read -p "The required module 'virtualenv' is not installed. Do you want to install it now? [y/n]" yn
-                case $yn in
-                [Yy]*) pip3 install virtualenv; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-
+    check_package "python3-dev"
+    check_package "libportaudio2"
+    check_package "python3-pip"
+    check_package "virtualenv"
 else
     #sudo installed
-    if [ $(dpkg-query -W -f='${Status}' python3-dev 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'python3-dev' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) sudo apt-get update && sudo apt-get install -y python3-dev; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if [ $(dpkg-query -W -f='${Status}' libportaudio2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'libportaudio2' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) sudo apt-get update && sudo apt-get install -y libportaudio2; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if [ $(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        while true; do
-            read -p "The required package 'python3-pip' is not installed. Do you want to install it now? [y/n]" yn
-            case $yn in
-                [Yy]*) sudo apt-get update && sudo apt-get install -y python3-pip; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
-    if ! python3 -c "import virtualenv" &> /dev/null; then
-        while true; do
-                read -p "The required module 'virtualenv' is not installed. Do you want to install it now? [y/n]" yn
-                case $yn in
-                [Yy]*) sudo pip3 install virtualenv; break ;;
-                [Nn]*) exit 0 ;;
-            esac
-        done
-    fi
+    check_package "python3-dev" "sudo"
+    check_package "libportaudio2" "sudo"
+    check_package "python3-pip" "sudo"
+    check_package "virtualenv" "sudo"
 fi
 
 virtualenv -p python3 .env
