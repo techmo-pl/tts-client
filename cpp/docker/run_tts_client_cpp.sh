b16868b14f43d94c5761d8cb2f1cec9bb67fa1bd
Jan Wozniak <jan.wozniak@techmo.pl>
2021-08-12 19:28:54 +0200

    correct help description

--- cpp/docker/run_tts_client_cpp.sh
+++ cpp/docker/run_tts_client_cpp.sh
@@ -35,7 +35,7 @@ Techmo TTS gRPC client ${IMAGE_VERSION}
   -o=OUTPUT_FILE, --output-file=OUTPUT_FILE
                         A custom name for output wave file with synthesized audio content (default: 'TechmoTTS.wav'). 
                         File will be generated inside 'wav' directory.
-  --tls                 Use SSL/TLS authentication. The credential files (client.crt, client.key, ca.crt) should be placed inside 'SSL/TLS' directory.
+  --tls                 Use SSL/TLS authentication. The credential files (client.crt, client.key, ca.crt) should be placed inside 'tls' directory.
   -f=SAMPLE_RATE, --sample-rate=SAMPLE_RATE
                         A sample rate in Hz of synthesized audio. Set to 0 (default) to use voice's original sample rate.
   --ae=AUDIO_ENCODING, --audio-encoding=AUDIO_ENCODING
